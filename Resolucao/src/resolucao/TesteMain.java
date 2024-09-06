a1a14e8d17dcbe84d93623bfae46b0cc2942f717
Matias G Adelina <98082081+matias2k2@users.noreply.github.com>
2024-09-06 13:11:52 +0100

    ajusteDoCodigo

--- Resolucao/src/resolucao/TesteMain.java
+++ Resolucao/src/resolucao/TesteMain.java
@@ -29,19 +29,33 @@ public class TesteMain {
         } else {
             System.out.println("O número " + numero + " não pertence à sequência de Fibonacci.");
         }
-        
+
         sc.nextLine();
 
-        //Exercicio 2
+        // Exercicio 2
         System.out.print("Informe uma string: ");
         String input = sc.nextLine();
 
         int count = test.countLestra(input);
         System.out.println("A letra 'a' aparece " + count + " vez(es) na string.");
-       
+
         System.out.println("Rosolucao do caso 3");
         test.SomaCalculo();
 
+        // Caso 4
+
+        System.out.println("a) 1, 3, 5, 7, 9");
+
+        System.out.println("b) 2, 4, 8, 16, 32, 64, 128");
+
+        System.out.println("c) 0, 1, 4, 9, 16, 25, 36, 49");
+
+        System.out.println("d) 4, 16, 36, 64, 100");
+
+        System.out.println("e) 1, 1, 2, 3, 5, 8, 13");
+
+        System.out.println("f) 2, 10, 12, 16, 17, 18, 19, 20");
+
     }
-    
+
 }
